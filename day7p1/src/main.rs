be14bbf8c544776d06f63fb96185995e9a8523e1
Jade Bilkey <425547+Fumon@users.noreply.github.com>
2022-12-07 04:18:51 -0500

    Having issues: Next step is to recursive compute sizes with files as a test

--- day7p1/src/main.rs
+++ day7p1/src/main.rs
@@ -1,102 +1,134 @@
-use core::panic;
-use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-    path::PathBuf,
-};
-
-use support::{CDArg, Command, Directory, FileSystem, Listing};
+use support::{FileSystem, Directory};
 
 fn main() -> std::io::Result<()> {
-    let mut line_iter = BufReader::new(File::open("./input")?).lines().flatten();
+    let filesystem = FileSystem::load_from_dir_walk_file("./input")?;
 
-    let combined_size = part_1(&mut line_iter);
+    let combined_size = part_1(&filesystem);
 
     println!("Combined size of candidate directories: {}", combined_size);
+
+    let directory_for_deletion = part_2(&filesystem, 30000000);
+
+    println!("Directory for deltion {:?}", directory_for_deletion);
 
     Ok(())
 }
 
-fn part_1(line_iter: &mut impl Iterator<Item = String>) -> u32 {
-    let mut p_line_iter = line_iter.peekable();
-
-    let mut dirs: FileSystem = Default::default();
-    let mut current_path: Option<PathBuf> = None;
-    let mut current_dir_size: u32 = 0;
-    loop {
-        // Parse Command
-        let Some(cline) = p_line_iter.next() else {
-            // Add to parents
-            while current_path.as_mut().map(|x| x.pop()).unwrap() {
-                dirs.add_to_size_of_directory(current_path.clone().unwrap(), current_dir_size);
-            }
-
-            break;
-        };
-        let command = match cline.parse::<Command>() {
-            Ok(command) => command,
-            Err(e) => panic!("Error parsing command: {}", e),
-        };
-
-        match command {
-            Command::CD(CDArg::Parent) => {
-                // Pop up
-                // Add to the directory's total
-                current_path.as_mut()
-                    .map(|x| {
-                        x.pop()
-                    })
-                    .expect("Only pop if not beyond root");
-
-                // Update sizes
-                current_dir_size =
-                    dirs.add_to_size_of_directory(current_path.clone().unwrap(), current_dir_size);
-            }
-            Command::CD(CDArg::Name(name)) => {
-                // Descend
-                let p = current_path.clone();
-                current_path
-                    .get_or_insert(PathBuf::new())
-                    .push(name.clone());
-
-                dirs.insert(Directory {
-                    parent: p,
-                    name,
-                    size: 0,
-                });
-
-                current_dir_size = 0;
-            }
-            Command::LS => {
-                // Consume until next command
-                while let Some(ls_line) =
-                    p_line_iter.next_if(|line| line.chars().nth(0) != Some('$'))
-                {
-                    let listing = match ls_line.parse::<Listing>() {
-                        Ok(listing) => listing,
-                        Err(e) => panic!("Error parsing listing: {}", e),
-                    };
-
-                    if let Listing::File(size, _) = listing {
-                        current_dir_size += size;
-                    }
-                }
-
-                // Update this directory
-                dirs.add_to_size_of_directory(current_path.clone().unwrap(), current_dir_size);
-            }
-        };
-    }
-
+fn part_1(filesystem: &FileSystem) -> u32 {
     // Sort and tally
-    dirs.get_dirs().filter_map(|dir| if dir.size <= 100000 { Some(dir.size)} else {None}).sum()
+    filesystem
+        .get_dirs()
+        .iter()
+        .filter_map(|dir| {
+            if dir.size <= 100000 {
+                Some(dir.size)
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+fn part_2(filesystem: &FileSystem, needed: u32) -> Directory {
+    
+    let target_to_free =compute_target_to_free(needed,filesystem.find_available_space());
+
+    println!("Target to free: {}", target_to_free);
+
+    let dirs = filesystem.get_dirs();
+    let mut candidates = dirs.iter().filter_map(|dir| {
+        if dir.size >= target_to_free {
+            Some(dir)
+        } else {
+            None
+        }
+    }).collect::<Vec<&Directory>>();
+
+    candidates.sort_unstable_by_key(|dir| dir.size);
+
+    for ele in candidates.iter().enumerate() {
+        println!("Candidate {: >3}: {:?}", ele.0, ele.1);
+    }
+    
+    candidates.iter().nth(0).copied().unwrap().clone()
+}
+
+fn compute_target_to_free(needed: u32, available: u32) -> u32 {
+    needed - available
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use std::{io::{Cursor, BufRead}, path::PathBuf};
+
+    use crate::{support::FileSystem, part_1, part_2, compute_target_to_free};
+
+    const EXAMPLE: &'static str = r"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+    
+    #[test]
+    fn part_1_test() {
+        let filesystem = load_example_filesystem();
+
+        let root = filesystem.get_dir(PathBuf::from("/")).unwrap();
+
+        assert_eq!(root.size, 48381165);
+        
+        assert_eq!(part_1(&filesystem), 95437);
+    }
+
+    #[test]
+    fn part_2_test() {
+        let filesystem = load_example_filesystem();
+
+        let needed = 30000000;
+        let available = filesystem.find_available_space();
+
+        assert_eq!(available, 21618835);
+
+        let target_to_free = compute_target_to_free(needed, available);
+        assert_eq!(target_to_free, 8381165);
+
+        let candidate = part_2(&filesystem, needed);
+
+        assert_eq!(candidate.name, "d");
+        assert_eq!(candidate.size, 24933642)
+    }
+
+    fn load_example_filesystem() -> FileSystem{
+        FileSystem::load_from_dir_walk(Cursor::new(EXAMPLE).lines().flatten()).unwrap()
+    }
+}
 
 mod support {
-    use std::{collections::HashMap, path::PathBuf, str::FromStr};
+    use std::{
+        collections::HashMap,
+        fs::File,
+        io::{BufRead, BufReader},
+        path::{Path, PathBuf},
+        str::FromStr,
+    };
 
     #[derive(Debug, Default)]
     pub(crate) struct FileSystem {
@@ -104,6 +136,8 @@ mod support {
     }
 
     impl FileSystem {
+        pub(crate) const TOTAL_DISK_SPACE: u32 = 70000000;
+
         /// Looks up and updates the size of a directory returning the directory's new size
         pub(crate) fn add_to_size_of_directory(
             &mut self,
@@ -118,6 +152,7 @@ mod support {
 
             newsize
         }
+
         pub(crate) fn insert(&mut self, d: Directory) {
             let mut binding = d.parent.clone();
             let newpath = binding.get_or_insert(PathBuf::new());
@@ -125,12 +160,112 @@ mod support {
 
             self.file_table.insert(newpath.to_owned(), d);
         }
-        pub(crate) fn get_dirs(self) -> impl Iterator<Item = Directory> {
-            self.file_table.into_iter().map(|(_, dir)| dir)
+
+        pub(crate) fn find_available_space(&self) -> u32{
+            Self::TOTAL_DISK_SPACE - self.get_dir(PathBuf::from("/")).unwrap().size
+        }
+
+        pub(crate) fn get_dirs(&self) -> Vec<Directory> {
+            self.file_table
+                .iter()
+                .map(|(_, dir)| dir)
+                .cloned()
+                .collect()
+        }
+
+        pub(crate) fn get_dir(&self, p: PathBuf) -> Option<&Directory> {
+            self.file_table.get(&p)
+        }
+
+        pub(crate) fn load_from_dir_walk(i: impl Iterator<Item = String>) -> std::io::Result<Self> {
+            let mut p_line_iter = i.peekable();
+
+            let mut dirs: FileSystem = Default::default();
+            let mut current_path: Option<PathBuf> = None;
+            let mut current_dir_size: u32 = 0;
+            loop {
+                // Parse Command
+                let Some(cline) = p_line_iter.next() else {
+                    // Add to parents
+                    while current_path.as_mut().map(|x| x.pop()).unwrap() {
+                        dirs.add_to_size_of_directory(current_path.clone().unwrap(), current_dir_size);
+                    }
+
+                    break;
+                };
+
+                let command = match cline.parse::<Command>() {
+                    Ok(command) => command,
+                    Err(e) => panic!("Error parsing command: {}", e),
+                };
+
+                match command {
+                    Command::CD(CDArg::Parent) => {
+                        // Pop up
+                        // Add to the directory's total
+                        current_path
+                            .as_mut()
+                            .map(|x| x.pop())
+                            .expect("Only pop if not beyond root");
+
+                        // Update sizes
+                        current_dir_size = dirs.add_to_size_of_directory(
+                            current_path.clone().unwrap(),
+                            current_dir_size,
+                        );
+                    }
+                    Command::CD(CDArg::Name(name)) => {
+                        // Descend
+                        let p = current_path.clone();
+                        current_path
+                            .get_or_insert(PathBuf::new())
+                            .push(name.clone());
+
+                        dirs.insert(Directory {
+                            parent: p,
+                            name,
+                            size: 0,
+                        });
+
+                        current_dir_size = 0;
+                    }
+                    Command::LS => {
+                        // Consume until next command
+                        while let Some(ls_line) =
+                            p_line_iter.next_if(|line| line.chars().nth(0) != Some('$'))
+                        {
+                            let listing = match ls_line.parse::<Listing>() {
+                                Ok(listing) => listing,
+                                Err(e) => panic!("Error parsing listing: {}", e),
+                            };
+
+                            if let Listing::File(size, _) = listing {
+                                current_dir_size += size;
+                            }
+                        }
+
+                        // Update this directory
+                        dirs.add_to_size_of_directory(
+                            current_path.clone().unwrap(),
+                            current_dir_size,
+                        );
+                    }
+                };
+            }
+            Ok(dirs)
+        }
+
+        pub(crate) fn load_from_dir_walk_file<P: AsRef<Path>>(
+            file_name: P,
+        ) -> std::io::Result<Self> {
+            let line_iter = BufReader::new(File::open(file_name)?)
+                .lines()
+                .flatten();
+            Self::load_from_dir_walk(line_iter)
         }
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub(crate) struct Directory {
         pub(crate) parent: Option<PathBuf>,
         pub(crate) name: String,
