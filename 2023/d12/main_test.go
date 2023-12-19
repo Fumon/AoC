dfbfcc0bdfa0c90c57f7f4e4b0f1a712021f3997
Jade Bilkey <425547+Fumon@users.noreply.github.com>
2023-12-19 13:05:33 -0500

    day12 update tests and benchmarks

--- 2023/d12/main_test.go
+++ 2023/d12/main_test.go
@@ -7,4 +7,17 @@ import (
 
 func TestPart1(t *testing.T) {
 	u.Assert(t, 21, part1(u.Linewisefile_chan("testinput")))
+	u.Assert(t, 7718, part1(u.Linewisefile_chan("input")))
+}
+
+func BenchmarkPart1TestInput(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part1(u.Linewisefile_chan("testinput"))
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part1(u.Linewisefile_chan("input"))
+	}
 }
