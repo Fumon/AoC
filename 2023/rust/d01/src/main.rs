187f92c252ce983104fa2c4f6c25c93988a9210d
Jade Bilkey <425547+Fumon@users.noreply.github.com>
2024-02-08 10:19:59 -0500

    day01p1 cleanup

--- 2023/rust/d01/src/main.rs
+++ 2023/rust/d01/src/main.rs
@@ -4,17 +4,21 @@ use std::{fs::File, io::{BufRead, BufReader}};
 
 fn main() {
     let bf = BufReader::new(File::open("input/input").unwrap());
+    
+    println!("{}", part1(bf.lines().into_iter().filter_map(Result::ok)));
+}
 
-    let mut digitsum: u32 = 0;
-    for line in bf.lines().into_iter().filter_map(Result::ok) {
-        let numbers: Vec<u32> = line.chars().filter_map(|ch| {
+fn part1(lines: impl IntoIterator<Item = impl AsRef<str>>) -> i64 {
+    let mut digitsum: i64 = 0;
+    for line in lines {
+        let numbers: Vec<_> = line.as_ref().chars().filter_map(|ch| {
             match ch {
-                '0'..='9' => Some(ch as u32 - '0' as u32),
+                '0'..='9' => Some(ch as i64 - '0' as i64),
                 _ => None
             }
         }).collect();
         digitsum += numbers.first().unwrap() * 10 + numbers.last().unwrap()
     }
 
-    println!("{}", digitsum);
+    return digitsum;
 }
